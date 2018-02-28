e0b3603908b4252e4d8851119ff0ee5554206f7f
Matt Shim (TEKsystems) <v-shimma@microsoft.com>
2018-02-28 14:24:22 -0800

    edited setup.py and qna_maker

--- libraries/botbuilder-ai/botbuilder/ai/qna_maker.py
+++ libraries/botbuilder-ai/botbuilder/ai/qna_maker.py
@@ -2,8 +2,8 @@ import json
 import asyncio
 import requests
 
-class QnAMaker:
 
+class QnAMaker(object):
     def __init__(self, options, http_client):
         self.__qnaMakerServiceEndpoint = 'https://westus.api.cognitive.microsoft.com/qnamaker/v3.0/knowledgebases/'
         self.__json_mime_type = 'application/json'
@@ -19,45 +19,48 @@ class QnAMaker:
         self.__answerUrl = "%s%s/generateanswer" % (self.__qnaMakerServiceEndpoint,options.knowledge_base_id)
 
         if self.__options.ScoreThreshold == 0:
-            self.__options.ScoreThreshold = 0.3 #Note - SHOULD BE 0.3F 'FLOAT'
+            self.__options.ScoreThreshold = 0.3  # Note - SHOULD BE 0.3F 'FLOAT'
         
         if self.__options.Top == 0:
             self.__options.Top = 1
         
-        if self.__options.StrictFilters == None:
+        if self.__options.StrictFilters is None:
             self.__options.StrictFilters = MetaData()
 
-        if self.__options.MetadataBoost == None:
+        if self.__options.MetadataBoost is None:
             self.__options.MetadataBoost = MetaData()
 
-    async def get_answers(question):        # HTTP call
+    async def get_answers(self, question):        # HTTP call
         headers = {
-            self.__api_management_header : self.__options.subscription_key,
-            "Content-Type" : __json_mime_type
+            self.__api_management_header: self.__options.subscription_key,
+            "Content-Type": self.__json_mime_type
         }
         
         payload = json.dumps({
-            "question" : question
+            "question": question
         })
         # POST request to QnA Service
         content = requests.post(self.__answerUrl, headers=headers, data=payload)
         return content.json()
-     
-class QnAMakerOptions:
-    def __init__(self, subscription_key, knowledge_base_id, score_threshhold, top, strict_filters, metadata_boost):
+
+
+class QnAMakerOptions(object):
+    def __init__(self, subscription_key, knowledge_base_id, score_threshold, top, strict_filters, metadata_boost):
         self.subscription_key = subscription_key
         self.knowledge_base_id = knowledge_base_id
-        self.score_threshhold = score_threshhold
+        self.score_threshold = score_threshold
         self.top = top
         self.strict_filters = strict_filters
         self.metadata_boost = metadata_boost
 
-class MetaData:
+
+class MetaData(object):
     def __init__(self, name, value):
         self.name = name
         self.value = value
 
-class QueryResult:
+
+class QueryResult(object):
     def __init__(self, questions, answer, score, metadata, source, qna_id):
         self.questions = questions
         self.answer = answer
@@ -66,6 +69,7 @@ class QueryResult:
         self.source = source
         self.qna_id = qna_id
 
-class QueryResults:
+
+class QueryResults(object):
     def __init__(self, answers):
         self.__answers = answers
--- libraries/botbuilder-ai/setup.py
+++ libraries/botbuilder-ai/setup.py
@@ -1,15 +1,22 @@
-from setuptools import setup, find_packages
+from setuptools import setup
+
+NAME = "botbuilder-ai"
+VERSION = "4.0.0.a0"
+REQUIRES = [
+    "botbuilder-schema>=4.0.0.a0",
+    "requests>=2.18.4"]
 
 setup(
-    name='microsoft-botbuilder-ai',
-    version='4.0.0-a0',
+    name=NAME,
+    version=VERSION,
     url='https://www.github.com/Microsoft/botbuilder-python',
     long_description='Cognitive services extensions for Microsoft BotBuilder.',
     license='MIT',
     author='Microsoft',
     author_email='bf-reports@microsoft.com',
     description='',
-    packages=find_packages(),
+    packages=["botbuilder.ai"],
+    install_requires=REQUIRES,
     classifiers=[
         'Programming Language :: Python',
         'Intended Audience :: Bot Developers',
