f0d316544f3a7b7668d27bec9da50435aa6c09fa
Santiago Grangetto <santiagograngetto@gmail.com>
2020-06-24 14:46:24 -0300

    add missing class

--- libraries/botbuilder-schema/botbuilder/schema/_models.py
+++ libraries/botbuilder-schema/botbuilder/schema/_models.py
@@ -1066,6 +1066,30 @@ class ConversationParameters(Model):
         self.tenant_id = kwargs.get("tenant_id", None)
 
 
+class ConversationResourceResponse(Model):
+    """A response containing a resource.	
+    :param activity_id: ID of the Activity (if sent)	
+    :type activity_id: str	
+    :param service_url: Service endpoint where operations concerning the	
+     conversation may be performed	
+    :type service_url: str	
+    :param id: Id of the resource	
+    :type id: str	
+    """
+
+    _attribute_map = {
+        "activity_id": {"key": "activityId", "type": "str"},
+        "service_url": {"key": "serviceUrl", "type": "str"},
+        "id": {"key": "id", "type": "str"},
+    }
+
+    def __init__(self, **kwargs):
+        super(ConversationResourceResponse, self).__init__(**kwargs)
+        self.activity_id = kwargs.get("activity_id", None)
+        self.service_url = kwargs.get("service_url", None)
+        self.id = kwargs.get("id", None)
+
+
 class ConversationsResult(Model):
     """Conversations result.
 
