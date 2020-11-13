32db7888accdb90ed909c0ee2c2a07ad3486841f
Ian Luca Scaltritti <ian.scaltritti@southworks.com>
2020-11-13 11:35:58 -0300

    .

--- libraries/functional-tests/slackbot/config.py
+++ libraries/functional-tests/slackbot/config.py
@@ -11,5 +11,5 @@ class DefaultConfig:
     PORT = 3978
 
     SLACK_VERIFICATION_TOKEN = os.environ.get("SlackVerificationToken", "16q9F4wbB9o2sMie2CREoQbg")
-    SLACK_BOT_TOKEN = os.environ.get("SlackBotToken", "xoxb-1493382945509-1502666884372-S6emLrmxCQrbLZYrz1pP7uPp")
+    SLACK_BOT_TOKEN = os.environ.get("SlackBotToken", "xoxb-1493382945509-1502666884372-L35SlSSZvIwZeBNdgm3msVo9")
     SLACK_CLIENT_SIGNING_SECRET = os.environ.get("SlackClientSigningSecret", "a52331e1bdf3b90667e9b7b3ffe8308e")
